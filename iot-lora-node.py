1fa544cd46898bf1f0009018e62c23bd950301c6
Ryan Walmsley <ryanteck@gmail.com>
2018-08-02 12:49:16 +0100

    Initial Template Created For Library

---
+++ iot-lora-node.py
@@ -0,0 +1,42 @@
+"""
+# IOT LoRa Node Python Library - Used to interface to RAK811 Boards with Micropython/Python 3.X
+# Copyright (C) 2018 Pi Supply
+# Written by Ryan Walmsley (Ryan@pi-supply.com)
+# Designed for Raspberry Pi, Beaglebone, ESP32 & Microbit
+"""
+
+#Import the serial library for the respective platform
+
+#Raspberry Pi
+
+#Beaglebone
+
+#ESP32
+
+#Microbit
+
+class iotloranode(object):
+    """RAK811 Interface Library - Converts inputs given to serial commands for RAK811"""
+
+    def __init__(self, region=1):
+        """Initialise The Library and connect to the module"""
+
+    ############
+    # UART Functions
+    ############
+
+    ############
+    # Set Settings Functions
+    ############
+
+    ############
+    # Get Settings Functions
+    ############
+
+    ############
+    # LoRa Transmit Functions
+    ############
+
+    ############
+    # GPIO Functions
+    ############
