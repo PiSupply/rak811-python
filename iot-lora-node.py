c69f9d2b7e9a3685132887555870757b4ea6e16e
Ryan Walmsley <ryanteck@gmail.com>
2018-08-02 14:17:31 +0100

    Believe the template for each command is there

--- iot-lora-node.py
+++ iot-lora-node.py
@@ -29,68 +29,68 @@ class iotloranode(object):
     ############
     # Set Settings Functions
     ############
-    def set_devAddr(self,address):
+    def set_devAddr(self,devAddr):
         """Set Device Address"""
-    def set_devEUI(self,address):
+    def set_devEUI(self,devEui):
         """Set Device EUI"""
-    def set_appEUI(self,address):
+    def set_appEUI(self,appEui):
         """Set Application EUI"""
-    def set_appKey(self,address):
+    def set_appKey(self,appKey):
         """Set Application Key"""
-    def set_networkKey(self,address):
+    def set_networkKey(self,nwsk):
         """Set Network Key"""
-    def set_appSessionKey(self,address):
+    def set_appSessionKey(self,apsk):
         """Set Application Session Key"""
-    def set_loraPower(self,address):
+    def set_loraPower(self,pwr):
         """Set Lora Power Level"""
-    def set_adrMode(self,address):
+    def set_adrMode(self,adr):
         """Set Adaptive Data Rate"""
-    def set_spreadingFactor(self,address):
+    def set_spreadingFactor(self,sf):
         """Set Spreading Factor"""
 
 
     ############
     # Get Settings Functions
     ############
-    def get_devAddr(self,address):
+    def get_devAddr(self):
         """Get Device Address"""
-    def get_devEUI(self,address):
+    def get_devEUI(self):
         """Get Device EUI"""
-    def get_appEUI(self,address):
+    def get_appEUI(self):
         """Get Application EUI"""
-    def get_appKey(self,address):
+    def get_appKey(self):
         """Get Application Key"""
-    def get_networkKey(self,address):
+    def get_networkKey(self):
         """Get Network Key"""
-    def get_appSessionKey(self,address):
+    def get_appSessionKey(self):
         """Get Application Session Key"""
-    def get_loraPower(self,address):
+    def get_loraPower(self):
         """Get Lora Power Level"""
-    def get_adrMode(self,address):
+    def get_adrMode(self):
         """Get Adaptive Data Rate"""
-    def get_spreadingFactor(self,address):
+    def get_spreadingFactor(self):
         """Get Spreading Factor"""
     ############
     # LoRa Functions
     ############
     def join_request(self):
         """Join over OTAA"""
-    def send_raw_packet(self,address):
+    def send_raw_packet(self,packet):
         """Send raw bytes packet"""
-    def sent_string_packet(self,address):
+    def sent_string_packet(self,string):
         """Send a string packet"""
-    def send_int_packet(self,address):
+    def send_int_packet(self,int):
         """Send integer packet"""
-    def recieve_packet(self,address):
+    def recieve_packet(self):
         """Check To See if there is any response"""
     ############
     # GPIO Functions
     ############
-    def gpio_setmode(self,address):
+    def gpio_setmode(self,pin,mode):
         """Set Pin GPIO Mode"""
-    def gpio_read(self,address):
-        """Set Device EUI"""
-    def gpio_write(self,address):
-        """Set Application EUI"""
-    def gpio_adc(self,address):
-        """Set Application Key"""   
+    def gpio_read(self,pin):
+        """Read RAK811 GPIO"""
+    def gpio_write(self,pin,state):
+        """RAK811 GPIO Write Command"""
+    def gpio_adc(self,pin):
+        """Read RAK811 ADC"""
